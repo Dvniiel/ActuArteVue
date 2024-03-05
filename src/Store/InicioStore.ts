6247f62b7aaa57bc98f688371579a98f98a0e2b3
Daniel Valdovinos <a24698@svalero.com>
2024-03-05 19:27:29 +0100

    Arregladas 3 obras del Inicio desde la BD

--- src/Store/InicioStore.ts
+++ src/Store/InicioStore.ts
@@ -10,12 +10,11 @@ export const useInicioStore = defineStore('inicio', {
     async fetchObras() {
       try {
         const response = await axios.get('http://localhost:8001/Obras');
-        // Suponiendo que tu API devuelve las obras en un orden específico y quieres los primeros 3
         this.obras = response.data.slice(0, 3);
       } catch (error) {
         console.error('Error al obtener las obras: ', error);
       }
     }
   }
-  
+
 });
