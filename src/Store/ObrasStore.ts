3445b80f2bae71e3f99319d153b48c44c3fb49d6
DESKTOP-S3CMJ72\acent <a26515@svalero.com>
2024-03-06 21:13:12 +0100

    Continuación del Dashboard

--- src/Store/ObrasStore.ts
+++ src/Store/ObrasStore.ts
@@ -1,5 +1,5 @@
-import { defineStore } from 'pinia';
-import axios from 'axios';
+import { defineStore } from "pinia";
+import axios from "axios";
 
 interface Obra {
   idObra: number;
@@ -7,20 +7,19 @@ interface Obra {
   imagen: string;
 }
 
-export const useObrasStore = defineStore('obras', {
-    
-    state: () => ({
-      obras: [] as Obra[],
-    }),
+export const useObrasStore = defineStore("obras", {
+  state: () => ({
+    obras: [] as Obra[],
+  }),
 
-    actions: {
-      async fetchObras() {
-        try {
-          const response = await axios.get('http://localhost:8001/Obras');
-          this.obras = response.data;
-        } catch (error) {
-          console.error('Hubo un error al obtener las obras: ', error);
-        }
+  actions: {
+    async fetchObras() {
+      try {
+        const response = await axios.get("http://localhost:8001/Obras");
+        this.obras = response.data;
+      } catch (error) {
+        console.error("Hubo un error al obtener las obras: ", error);
       }
-    }
+    },
+  },
 });
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,61 +1,141 @@
 <script setup lang="ts">
+import { ref, computed, onMounted } from "vue";
+import { useObrasStore, type Obra } from "@/store/DashboardStore";
+
+const obrasStore = useObrasStore();
+onMounted(() => {
+  obrasStore.fetchObras();
+});
+
+const defaultObra = {
+  nombreObra: "",
+  valoracionObra: 0,
+  autorObra: "",
+  directorObra: "",
+  duracionObra: "",
+  descObra: "",
+  imagen: "",
+};
+
+const obraSeleccionada = ref({ ...defaultObra });
+const modoFormulario = ref("");
+
+const obras = computed(() => obrasStore.obras);
+
+function handleDelete(idObra: number) {
+  obrasStore.deleteObra(idObra);
+}
+
+function handleAdd() {
+  obraSeleccionada.value = { ...defaultObra };
+  modoFormulario.value = "añadir";
+}
+
+function handleEdit(obra: Obra) {
+  obraSeleccionada.value = { ...obra };
+  modoFormulario.value = "editar";
+}
+
+function handleSubmit() {
+  if (modoFormulario.value === "añadir") {
+    obrasStore.addObra({ ...obraSeleccionada.value });
+  } else if (modoFormulario.value === "editar") {
+    obrasStore.updateObra(obraSeleccionada.value);
+  }
+  obraSeleccionada.value = { ...defaultObra };
+  modoFormulario.value = "";
+}
+
+function handleCancel() {
+  obraSeleccionada.value = { ...defaultObra };
+  modoFormulario.value = "";
+}
 </script>
+
 <template>
-    <div class="container-dashboard">
+  <div class="container-dashboard">
+    <main>
+      <button @click="handleAdd" class="btn-añadir-obras">Añadir Obra</button>
+      <div class="recent_order">
+        <h1>Listado de Obras</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Título</th>
+              <th>ID de la Obra</th>
+              <th>Edit</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="obra in obras" :key="obra.idObra">
+              <td>{{ obra.nombreObra }}</td>
+              <td>{{ obra.idObra }}</td>
+              <td class="primary" @click="handleEdit(obra)">
+                <i class="ri-pencil-fill"></i>
+              </td>
+              <td
+                class="primary"
+                @click="obra.idObra && handleDelete(obra.idObra)"
+              >
+                <i class="ri-delete-bin-line"></i>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
 
-        <main>
-
-            <div class="recent_order">
-                <h1>Listado de Obras</h1>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Título</th>
-                            <th>ID de la Obra</th>
-                            <th>Edit</th>
-                            <th>Delete</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-
-                        <tr>
-                            <td>Cats</td>
-                            <td>2</td>
-                            <td class="primary"><i class="ri-pencil-fill"></i></td>
-                            <td class="primary"><i class="ri-delete-bin-line"></i></td>
-                        </tr>
-                        <tr>
-                            <td>Wicks</td>
-                            <td>6</td>
-                            <td class="primary"><i class="ri-pencil-fill"></i></td>
-                            <td class="primary"><i class="ri-delete-bin-line"></i></td>
-                        </tr>
-                        <tr>
-                            <td>Don Juan Tenorio</td>
-                            <td>8</td>
-                            <td class="primary"><i class="ri-pencil-fill"></i></td>
-                            <td class="primary"><i class="ri-delete-bin-line"></i></td>
-                        </tr>
-                        <tr>
-                            <td>La Celestina</td>
-                            <td>9</td>
-                            <td class="primary"><i class="ri-pencil-fill"></i></td>
-                            <td class="primary"><i class="ri-delete-bin-line"></i></td>
-                        </tr>
-
-                    </tbody>
-                </table>
-            </div>
-
-            <!--end pedido reciente-->
-
-
-
-        </main>
-        <!-- main section end -->
-
-    </div>
-    <br><br>
+      <!-- Formulario para añadir o editar obras -->
+      <div
+        v-if="
+          obraSeleccionada &&
+          (modoFormulario === 'añadir' || modoFormulario === 'editar')
+        "
+        class="form-container"
+      >
+        <form @submit.prevent="handleSubmit">
+          <input
+            v-model="obraSeleccionada.nombreObra"
+            placeholder="Nombre de la obra"
+            required
+          />
+          <input
+            v-model="obraSeleccionada.descObra"
+            placeholder="Descripción de la obra"
+            required
+          />
+          <input
+            v-model="obraSeleccionada.valoracionObra"
+            placeholder="Valoración de la obra"
+            type="number"
+            required
+          />
+          <input
+            v-model="obraSeleccionada.directorObra"
+            placeholder="Director de la obra"
+            required
+          />
+          <input
+            v-model="obraSeleccionada.autorObra"
+            placeholder="Autor de la obra"
+            required
+          />
+          <input
+            v-model="obraSeleccionada.imagen"
+            placeholder="Imagen de la obra"
+            required
+          />
+          <input
+            v-model="obraSeleccionada.duracionObra"
+            placeholder="Duración de la obra"
+            required
+          />
+          <button type="submit">Guardar</button>
+          <button type="button" @click="handleCancel">Cancelar</button>
+        </form>
+      </div>
+    </main>
+  </div>
 </template>
 
 
@@ -65,281 +145,200 @@
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800&display=swap");
 
-
 :root {
-    --clr-primary: #896a4c;
-    --clr-success: #41f1b6;
-    --clr-white: #fff;
-    --clr-info-dark: #7d8da1;
-    --clr-info-light: #dce1eb;
-    --clr-dark: #363949;
-    --clr-warnig: #ff4e4e;
-    --clr-light: rgba(132, 139, 200, 0.18);
-    --clr-dark-variant: #677483;
-    --clr-color-background: #f6f6f9;
+  --clr-primary: #896a4c;
+  --clr-success: #41f1b6;
+  --clr-white: #fff;
+  --clr-info-dark: #7d8da1;
+  --clr-info-light: #dce1eb;
+  --clr-dark: #363949;
+  --clr-warnig: #ff4e4e;
+  --clr-light: rgba(132, 139, 200, 0.18);
+  --clr-dark-variant: #677483;
+  --clr-color-background: #f6f6f9;
 
-    --card-border-radius: 2rem;
-    --border-radius-1: 0.4rem;
-    --border-radius-2: 0.8rem;
-    --border-radius-3: 1.2rem;
+  --card-border-radius: 2rem;
+  --border-radius-1: 0.4rem;
+  --border-radius-2: 0.8rem;
+  --border-radius-3: 1.2rem;
 
-    --card-padding: 1.8rem;
-    --padding-1: 1.2rem;
-    --box-shadow: 0 2rem 3rem var(--clr-light);
+  --card-padding: 1.8rem;
+  --padding-1: 1.2rem;
+  --box-shadow: 0 2rem 3rem var(--clr-light);
 }
 
-*{
-
-    box-sizing: border-box;
-    text-decoration: none;
-    border: 0;
-    list-style: none;
-    appearance: none;
+* {
+  box-sizing: border-box;
+  text-decoration: none;
+  border: 0;
+  list-style: none;
+  appearance: none;
 }
 
-body{
-    font-family: 'Poppins' ,sans-serif;
-    width: 100%;
-    height: 100vh;
-    font-size: 0.88rem;
+body {
+  font-family: "Poppins", sans-serif;
+  width: 100%;
+  height: 100vh;
+  font-size: 0.88rem;
 }
 
-
-a{
-    color: var(--clr-dark);
+a {
+  color: var(--clr-dark);
 }
 
-h1{
-    font-weight: 800;
-    font-size: 1.8rem;
+h1 {
+  font-weight: 800;
+  font-size: 1.8rem;
 }
 
-h2{
-    font-size: 1.4rem;
+h2 {
+  font-size: 1.4rem;
 }
 
-h3{
-    font-size: 0.87rem;
+h3 {
+  font-size: 0.87rem;
 }
 
-h4{
-    font-size: 0.8;
+h4 {
+  font-size: 0.8;
 }
 
-h5{
-    font-size: 0.77rem;
+h5 {
+  font-size: 0.77rem;
 }
 
-small{
-    font-size: 0.75rem;
+small {
+  font-size: 0.75rem;
 }
 
-
-p{
-    color: var(--clr-dark-variant);
+p {
+  color: var(--clr-dark-variant);
 }
 
-b{
-    color: var(--clr-dark);
+b {
+  color: var(--clr-dark);
 }
-
-
 
 /*-------------MAIN SECTION--------------*/
 
-.container-dashboard{
-    display: flex;
-    text-align: center;
-    justify-content: center;
-    width: 100%;
+.container-dashboard {
+  margin-top: 20rem;
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  position: relative;
+  width: 60%;
+  transform: translate(30%, 0%);
 }
 
-main{
-    width: 100%;
-    text-align: center;
-    justify-content: center;
+main {
+  width: 100%;
+  text-align: center;
+  justify-content: center;
 }
 
-main input{
-    background-color: transparent;
-    border: 0;
-    outline: 0;
-    color: var(--clr-dark);
+main input {
+  background-color: transparent;
+  border: 0;
+  outline: 0;
+  color: var(--clr-dark);
 }
 
-main h1{
-    color: var(--clr-dark);
+main h1 {
+  color: var(--clr-dark);
 }
 
-
-main .recent_order{
-    margin-top: 2rem;
+main .recent_order {
+  margin-top: 2rem;
 }
 
-main .recent_order h2{
-    color: var(--clr-dark);
+main .recent_order h2 {
+  color: var(--clr-dark);
 }
 
-main .recent_order table{
-    background-color: var(--clr-white);
-    width: 100%;
-    border-radius: var(--card-border-radius);
-    padding: var(--card-padding);
-    text-align: center;
-    box-shadow: var(--box-shadow);
-    transition: all .3s ease;
-    color: var(--clr-dark);
+main .recent_order table {
+  background-color: var(--clr-white);
+  width: 100%;
+  border-radius: var(--card-border-radius);
+  padding: var(--card-padding);
+  text-align: center;
+  box-shadow: var(--box-shadow);
+  transition: all 0.3s ease;
+  color: var(--clr-dark);
 }
 
-main .recent_order table:hover{
-    box-shadow: none;
+main .recent_order table:hover {
+  box-shadow: none;
 }
 
-main table tbody tr{
-    height: 3.8rem;
-    border-bottom: 1px solid var(--clr-white);
-    color: var(--clr-dark-variant);
+main table tbody tr {
+  height: 3.8rem;
+  border-bottom: 1px solid var(--clr-white);
+  color: var(--clr-dark-variant);
 }
 
-main table tbody td{
-    height: 3.8rem;
-    border-bottom: 1px solid var(--clr-dark);
+main table tbody td {
+  height: 3.8rem;
+  border-bottom: 1px solid var(--clr-dark);
 }
 
-main table tbody tr:last-child td{
-    border: none;
+main table tbody tr:last-child td {
+  border: none;
 }
 
-main .recent_order a{
-    text-align: center;
-    display: block;
-    margin: 1rem;
+main .recent_order a {
+  text-align: center;
+  display: block;
+  margin: 1rem;
 }
 
-
-
-
-/*-------------------------MEDIA QUERY--------------------------*/
-
-@media screen and (max-width:1200px) {
-    .container{
-        width: 94%;
-        grid-template-columns: 7rem auto 14rem;
-    }
-
-    aside .sidebar h3{
-        display: none;
-    }
-
-    aside .sidebar a:last-child{
-        position: relative;
-        margin-top: 1.8rem;
-    }
-
-    main .insights{
-        display: grid;
-        grid-template-columns: repeat(1,1fr);
-    }
+.form-container {
+  max-width: 500px;
+  margin: 2rem auto;
+  padding: 2rem;
+  background-color: #fff;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
 }
 
-/*---------------------MEDIA QUERY PHONES ----------------------*/
-
-@media screen and (max-width:768px) {
-    .container{
-        width: 100%;
-        grid-template-columns: repeat(1,1fr);
-    }
-
-    aside{
-        position: fixed;
-        width: 18rem;
-        z-index: 3;
-        background: var(--clr-white);
-        display: none;
-        left: -100px;
-        animation: menuAni 1s forwards;
-    }
-
-    @keyframes menuAni {
-        to{
-            left: 0;
-        }
-    }
-
-    aside .logo h2{
-        display: inline;
-    }
-
-    aside .sidebar h3{
-        display: inline;
-    }
-
-    aside .sidebar a{
-        width: 100%;
-        height: 3.4rem;
-    }
-
-    aside .top div.close span{
-        display: inline;
-        position: absolute;
-        right: 0;
-        margin-right: 30px;
-        font-size: 35px;
-    }
-
-    .right .top{
-        position: fixed;
-        top: 0;
-        left: 0;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        padding: 0 0.8rem;
-        background: var(--clr-white);
-        width: 100%;
-        height: 4.6rem;
-        z-index: 2;
-        box-shadow: 0 1rem 1rem var(--box-shadow);
-        margin: 0;
-    }
-
-    main .insights{
-        display: grid;
-        grid-template-columns: repeat(1,1fr);
-        gap: 1.6rem;
-        padding: 40px;
-    }
-
-    main .recent_order{
-        padding: 30px;
-        margin: 0 auto;
-    }
-
-    .right .profile{
-        position: absolute;
-        left: 70%;
-    }
-
-    .right .recent_updates{
-        padding: 30px;
-    }
-
-    .right .sales_analytics{
-        padding: 30px;
-    }
-
-    .add_products{
-        padding: 30px;
-    }
-
-    .right .top button{
-        display: inline-block;
-        background: transparent;
-        cursor: pointer;
-        color: var(--clr-dark);
-        position: absolute;
-        left: 1rem;
-    }
+.form-group {
+  margin-bottom: 1rem;
 }
 
+.form-group label {
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+.form-group input[type="text"],
+.form-group input[type="number"],
+.form-group input[type="url"] {
+  width: 100%;
+  padding: 0.75rem;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+}
+
+.form-actions {
+  text-align: right;
+  padding-top: 1rem;
+}
+
+.btn-añadir-obras {
+  padding: 0.5rem 1rem;
+  margin-left: 0.5rem;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  background-color: var(--clr-primary);
+  color: white;
+}
+
+.btn-añadir-obras:hover {
+  opacity: 0.9;
+}
+
+.btn-añadir-obras:disabled {
+  background-color: #ccc;
+}
 </style>
